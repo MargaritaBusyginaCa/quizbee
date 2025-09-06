0a5720016f0b12e881a6e8642789631af2fc43b1
MargaritaBusyginaCa <busygina.rita.ca@gmail.com>
2025-09-06 12:27:55 -0400

    add QuizForm component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { QuizForm } from "@/components/QuizForm";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-rose-50">
-      <h1>Hello World!</h1>
+    <div className="w-full h-screen bg-rose-50 flex items-center justify-center">
+      <div>
+        <h1 className="py-3">Welcome to QuizBee 🐝</h1>
+        <QuizForm />
+      </div>
     </div>
   );
 }
---
+++ components/QuizForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import React, { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  quizPrompt: z.string().min(2).max(100),
+  numberOfQuestions: z.number().min(1).max(25),
+  difficulty: z.enum(["easy", "medium", "hard"]),
+});
+
+export function QuizForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      quizPrompt: "",
+      numberOfQuestions: 5,
+      difficulty: "easy",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className="flex justify-center items-center">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-fit mx-auto"
+        >
+          <FormField
+            control={form.control}
+            name="quizPrompt"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Quiz Prompt</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter your quiz prompt"
+                    {...field}
+                    className="w-fit"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
